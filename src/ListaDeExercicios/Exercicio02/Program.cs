173e0dfcd9d41499f3a3891d9f5c634441935cef
George Hamann <george.hamann@gmail.com>
2024-03-28 19:17:16 -0300

    Exemplo 02

---
+++ src/ListaDeExercicios/Exercicio02/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Converter a temperatura de graus Fahrenheit para graus Celsius
+             */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
